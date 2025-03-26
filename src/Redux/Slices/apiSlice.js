216aa41cd385d906f502461937278589b6e49549
donya ahmed <dodoaboshama0@gmail.com>
2025-03-27 00:37:59 +0200

    add env file

--- src/Redux/Slices/apiSlice.js
+++ src/Redux/Slices/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://fakestoreapi.com/",
+    baseUrl:  process.env.NEXT_PUBLIC_BASE_URL,
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
